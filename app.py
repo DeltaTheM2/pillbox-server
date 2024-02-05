5f7d279a8b189ac3694acd8b1179ac352fada1b6
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-02-04 17:40:12 -0800

    update

--- app.py
+++ app.py
@@ -17,12 +17,9 @@ collection = db.collection('pills')
 
 @app.route('/update_firestore', methods = ['POST'])
 def update_firestore():
-    data = request.json
-    print(data.get('med_history'))
-    print('data from client:', data)
     #print("Request Headers:", request.headers)
-    #json_data = request.get_json()
-    db.collection("pills").document().set(data)
+    json_data = request.get_json()
+    db.collection("pills").document().set(json_data)
     return {"status": "success", "message": "Data updated in Firestore"}, 200
     
 @app.route('/update_pill', methods = ['POST'])
@@ -52,8 +49,31 @@ def get_pill(uid):
     docsToReturn.append(doc.to_dict())
 
   return docsToReturn
+       
+  # collection_name = request.args.get('pills')
+  # document_name = request.args.get('uid')
+
+  # if not collection_name:
+  #   return "Missing collection or document name ", 400
+
+  # try:
+  #   doc_ref = db.collection(collection_name)
+  #   doc = doc_ref.get()
+  #   if doc.exists:
+  #     return doc.to_dic(), 200
+  #   else:
+  #     return "Document not found", 404
+
+  # except Exception as e:
+  #   return f"an error occured: {str(e)}", 500
 
 
+firebasedata = {
+      'med_count': 0,
+      'med_history': "2023-1-1T07:22Z",
+      'med_name': "somethingnewnew",
+      'reminder' : 0
+      }
  
 print("data sent")
 if __name__ == '__main__':
